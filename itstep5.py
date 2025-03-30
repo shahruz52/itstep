406f4878627af5ee06ed9f2a439635785aa6bc7a
shahruz52 <shahruzitachi@gmail.com>
2025-03-31 03:01:36 +0500

    Add files via upload

---
+++ itstep5.py
@@ -0,0 +1,21 @@
+# Intro_list = []
+# for method in dir(Intro_list):
+#     print(method)
+# ----------------------------------------------------------
+
+# for metod in dir():
+#     print(metod)
+# ------------------------------------------------------------
+
+# data = "string"
+# print(hasattr(data,"reverse"))
+# print(hasattr(data,"index"))
+# ------------------------------------------------------------
+
+# for _ in dir(__builtins__):
+#     print(_)
+# ---------------------------------------------------------------
+
+import sys
+
+print(sys.argv)
